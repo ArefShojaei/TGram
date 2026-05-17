66244e136e82b41dc7af4b245ac431406f42d28e
ArefShojaei <arefshojaei82@gmail.com>
2026-05-17 22:52:15 +0330

    chore: add listener state as an Ability

---
+++ src/Abilities/CanProvideListenerState.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace TGram\Abilities;
+
+trait CanProvideListenerState
+{
+    public function getCommandList(): array
+    {
+        return $this->commands;
+    }
+
+    public function getHears(): array
+    {
+        return $this->hears;
+    }
+
+    public function getEvents(): array
+    {
+        return $this->events;
+    }
+
+    public function getFallback(): ?object
+    {
+        return $this->fallback;
+    }
+}
