cc1fc7f0da106443d8b42ebc32fe6bd3ecee3600
ArefShojaei <arefshojaei82@gmail.com>
2026-05-25 14:36:54 +0330

    chore: update Command listener interface

--- src/Interfaces/Command/CommandListener.php
+++ src/Interfaces/Command/CommandListener.php
@@ -7,15 +7,15 @@ use TGram\Enums\MediaType;
 
 interface CommandListener
 {
-    public function command(string $command, Closure $handler): void;
+    public function command(string $command, Closure|array $handler): void;
 
-    public function hears(string $pattern, Closure $handler): void;
+    public function hears(string $pattern, Closure|array $handler): void;
 
-    public function on(MediaType $event, Closure $handler): void;
+    public function on(MediaType $event, Closure|array $handler): void;
 
     public function callback(Closure $handler): void;
 
-    public function start(Closure $handler): void;
+    public function start(Closure|array $handler): void;
 
-    public function help(Closure $handler): void;
+    public function help(Closure|array $handler): void;
 }
