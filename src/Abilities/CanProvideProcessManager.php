fc4afd07f3dcda39cd191439c42ffcbf7f032c36
ArefShojaei <arefshojaei82@gmail.com>
2026-06-25 16:55:25 +0330

    refactor: add the update processor for Polling & Webhook logics

--- src/Abilities/CanProvideProcessManager.php
+++ src/Abilities/CanProvideProcessManager.php
@@ -2,47 +2,90 @@
 
 namespace TGram\Abilities;
 
+use Throwable;
+
+use TGram\Utils\Settings;
+use TGram\Enums\HttpStatusCode;
+use TGram\Validators\WebhookRequestValidator;
 use TGram\Resolvers\{CallbackResolver, MessageResolver};
+use TGram\Exceptions\{HttpBodyRawException, UpdateException};
 
 trait CanProvideProcessManager
 {
+    private const CALLBACK_QUERY_PROPERTY = "callback_query";
+
+    private const MESSAGE_PROPERTY = "message";
+
     private function runPolling(): void
     {
-        $offset = 0;
+        $interval = Settings::get("polling.interval", 1);
+
+        $offset = Settings::get("polling.offset", 0);
 
         while (true) {
             $response = $this->getUpdates([
-                "query" => ["offset" => $offset],
+                "query" => [
+                    "offset" => $offset,
+                    "limit" => Settings::get("polling.limit", 0),
+                    "timeout" => Settings::get("polling.timeout", 0),
+                ],
             ]);
 
             $updates = $response->result;
 
-            if (!count($updates) || !is_array($updates)) continue;
+            if (empty($updates) || !is_array($updates)) continue;
 
             foreach ($updates as $update) {
                 $offset = $update->update_id + 1;
 
-                if (property_exists($update, "callback_query")) {
-                    $callbackResolver = new CallbackResolver($update, $this);
-
-                    $callbackResolver->dispatch();
-                }
-
-                if (property_exists($update, "message")) {
-                    $messageResolver = new MessageResolver($update, $this);
-
-                    $messageResolver->dispatch();
-                }
+                $this->processUpdate($update);
             }
 
-            $PER_ONE_SECOND = 1;
-
-            sleep($PER_ONE_SECOND);
+            sleep($interval);
         }
     }
 
     private function runWebhook(): void
     {
-        // TODO: Webhook logic code ...
+        $validator = new WebhookRequestValidator;
+
+        if (!$validator->validate()) {
+            http_response_code(HttpStatusCode::FORBIDDEN->value);
+
+            return;
+        }
+
+        try {
+            $bodyRaw = file_get_contents("php://input");
+
+            if (!$bodyRaw) throw new HttpBodyRawException();
+
+            $update = json_decode($bodyRaw);
+
+            if (!$update) throw new UpdateException();
+
+            $this->processUpdate($update);
+
+            http_response_code(HttpStatusCode::OK->value);
+        } catch (Throwable $error) {
+            http_response_code(HttpStatusCode::INTERNAL_SERVER_ERROR->value);
+
+            throw $error;
+        }
+    }
+
+    private function processUpdate(object $update): void
+    {
+        if (property_exists($update, self::CALLBACK_QUERY_PROPERTY)) {
+            $callbackResolver = new CallbackResolver($update, $this);
+
+            $callbackResolver->dispatch();
+        }
+
+        if (property_exists($update, self::MESSAGE_PROPERTY)) {
+            $messageResolver = new MessageResolver($update, $this);
+
+            $messageResolver->dispatch();
+        }
     }
 }
