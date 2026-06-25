ba96aeccc39f340acfcb6416071f8ac92e94c571
ArefShojaei <arefshojaei82@gmail.com>
2026-06-25 16:56:10 +0330

    chore: add Bot management APIs

--- src/Interfaces/Bot.php
+++ src/Interfaces/Bot.php
@@ -2,4 +2,38 @@
 
 namespace TGram\Interfaces;
 
-interface Bot extends Http {}
+interface Bot extends Http
+{
+    public function close(): object;
+
+    public function logOut(): object;
+
+    public function setName(
+        string $name,
+        ?string $language_code = null,
+    ): object;
+
+    public function getName(?string $language_code = null): object;
+
+    public function setDescription(
+        string $description,
+        ?string $language_code = null,
+    ): object;
+
+    public function getDescription(?string $language_code = null): object;
+
+    public function setShortDescription(
+        string $short_description,
+        ?string $language_code = null,
+    ): object;
+
+    public function getShortDescription(?string $language_code = null): object;
+
+    public function getDefaultAdministratorRights(
+        ?bool $for_channels = null,
+    ): object;
+
+    public function deleteDefaultAdministratorRights(
+        ?bool $for_channels = null,
+    ): object;
+}
