5cb2c7deae263c1931ccfa2c373c9496800ba9fd
ArefShojaei <arefshojaei82@gmail.com>
2026-06-25 16:58:59 +0330

    chore: update contracts

--- src/Interfaces/Settings.php
+++ src/Interfaces/Settings.php
@@ -6,7 +6,7 @@ interface Settings
 {
     public static function set(array $settings): void;
 
-    public static function get(string $key): mixed;
+    public static function get(string $key, mixed $default = null): mixed;
 
     public static function getAll(): array;
 }
--- src/Interfaces/Telegram.php
+++ src/Interfaces/Telegram.php
@@ -2,9 +2,7 @@
 
 namespace TGram\Interfaces;
 
-use TGram\Enums\ProcessMode;
 use TGram\Interfaces\Command\{
-    BotManagement,
     CommandListener,
     CommandListenerState,
     CommandManager,
@@ -14,10 +12,9 @@ interface Telegram extends
     CommandManager,
     CommandListener,
     CommandListenerState,
-    Webhook,
-    BotManagement
+    Webhook
 {
     public function configure(array $settings): void;
 
-    public function run(ProcessMode $mode = ProcessMode::POLLING): void;
+    public function run(): void;
 }
