62dcfc6f0226dc62c67032936d89d1bc61cd5b06
ArefShojaei <arefshojaei82@gmail.com>
2026-06-26 11:42:06 +0330

    chore: add validations

--- src/Telegram.php
+++ src/Telegram.php
@@ -6,6 +6,7 @@ use TGram\Enums\ProcessMode;
 use TGram\Utils\{Console, Settings};
 use TGram\Interfaces\Telegram as ITelegram;
 use TGram\Validators\BotTokenValidator;
+use TGram\Exceptions\EmptyArrayException;
 use TGram\Abilities\{
     CanProvideCommandManager,
     CanProvideListener,
@@ -31,6 +32,8 @@ final class Telegram extends Bot implements ITelegram
 
     public function configure(array $settings): void
     {
+        if (empty($settings)) throw new EmptyArrayException("Settings cannot be empty!");
+
         Settings::set($settings);
     }
 
@@ -38,7 +41,7 @@ final class Telegram extends Bot implements ITelegram
     {
         echo Console::info("Bot is running...") . PHP_EOL;
 
-        $driver = Settings::get("transport.driver");
+        $driver = Settings::get("transport.driver", ProcessMode::POLLING);
 
         match ($driver) {
             ProcessMode::POLLING => $this->runPolling(),
