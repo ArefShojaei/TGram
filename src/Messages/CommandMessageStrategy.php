437210a7d11572b5698b938e6c894128d495ef17
ArefShojaei <arefshojaei82@gmail.com>
2026-07-10 11:35:03 +0330

    feat: add command params

--- src/Messages/CommandMessageStrategy.php
+++ src/Messages/CommandMessageStrategy.php
@@ -11,6 +11,8 @@ use TGram\Interfaces\Message\MessageStrategy;
 final class CommandMessageStrategy extends MessageExecutor implements
     MessageStrategy
 {
+    private const COMMAND_WITH_PARAMETER_COUNT = 2;
+
     public function __construct(
         private string $input,
         private array $commands,
@@ -18,17 +20,50 @@ final class CommandMessageStrategy extends MessageExecutor implements
 
     public function handle(Context $context): void
     {
-        $command = ltrim($this->input, "/");
+        $commandInput = ltrim($this->input, "/");
 
         $fallbackMessage = Settings::get(
             "fallback_messages.command",
             FallbackMessage::COMMAND->value,
         );
 
-        $handler =
-            $this->commands[$command] ??
-            fn(Context $ctx) => $ctx->sendMessage($fallbackMessage);
+        foreach ($this->commands as $command => $handler) {
+            $pattern =
+                "/^" .
+                str_replace(
+                    ["{", "}"],
+                    ["(?<", ">[\w\/\:\.\@\?\_\-]+)"],
+                    $command,
+                ) .
+                "$/";
+
+            preg_match($pattern, $commandInput, $matches);
+
+            if (count($matches)) break;
+        }
+
+        if (empty($matches)) {
+            $handler = $this->getFallbackHandler($fallbackMessage);
+        }
+
+        if (
+            isset($matches) &&
+            count($matches) >= self::COMMAND_WITH_PARAMETER_COUNT
+        ) {
+            $this->addCommandParams($context, $matches);
+        }
 
         $this->execute($handler, $context);
+    }
+
+    private function addCommandParams(Context $context, array $matches): void
+    {
+        $params = array_filter(
+            $matches,
+            fn($key) => is_string($key),
+            ARRAY_FILTER_USE_KEY,
+        );
+
+        $context->update->message->params = $params;
     }
 }
